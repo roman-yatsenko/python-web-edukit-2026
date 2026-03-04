417b6fc54a6230f626cd951832df090cc039ea39
YaRo <roman.yatsenko@hneu.net>
2026-03-04 21:04:19 +0200

    Add Content model and related content types for modular course structure

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
@@ -41,3 +43,43 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name="contents", on_delete=models.CASCADE, verbose_name="Тема")
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, verbose_name="Тип контенту",
+                                     limit_choices_to={'model__in': (
+                                         'text',
+                                         'file',
+                                         'video',
+                                         'image',
+                                         'url',
+                                     )})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+class BaseContent(models.Model):
+    owner = models.ForeignKey(User, related_name="%(class)s_related", on_delete=models.CASCADE, verbose_name="Автор")
+    title = models.CharField(max_length=500, verbose_name="Назва")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+class Text(BaseContent):
+    content = models.TextField(verbose_name="Текст")
+
+class File(BaseContent):
+    file = models.FileField(upload_to="files", verbose_name="Файл")
+
+class Image(BaseContent):
+    file = models.FileField(upload_to="images", verbose_name="Зображення")
+
+class Video(BaseContent):
+    url = models.URLField(verbose_name="Відео")
+
+class URL(BaseContent):
+    url = models.URLField(verbose_name="Лінк")
