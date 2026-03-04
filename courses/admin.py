5ab4a1536a9068830d8151cc3bb7b650bfba09c8
YaRo <roman.yatsenko@hneu.net>
2026-03-04 20:15:23 +0200

    Register Course and Subject models in admin with custom configurations

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course, Subject
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title', )}
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title', )}
