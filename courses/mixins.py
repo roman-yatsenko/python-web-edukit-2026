b00d9d27c9cbe1d76fce32cab39a4c8cd0f513b4
YaRo <roman.yatsenko@hneu.net>
2026-03-11 21:07:17 +0200

    Add ManageCourseListView and Owner mixins for course management

---
+++ courses/mixins.py
@@ -0,0 +1,25 @@
+from django.urls import reverse_lazy
+
+from .models import Course
+
+
+class OwnerMixin:
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+    
+
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+    
+
+class OwnerCourseMixin(OwnerMixin):
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    template_name = 'courses/manage/course/form.html'
