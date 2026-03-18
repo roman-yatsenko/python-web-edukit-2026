e7cc9a0b0103cf81dd7e9ab0255f60b0fba1dc3c
YaRo <roman.yatsenko@hneu.net>
2026-03-18 19:13:44 +0200

    Refactor ManageCourseListView to include OwnerCourseMixin and add CourseCreateView, CourseUpdateView, and CourseDeleteView classes

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 
+from .mixins import OwnerCourseEditMixin, OwnerCourseMixin
 from .models import Course
 
 
@@ -9,11 +11,18 @@ class HomeView(TemplateView):
     template_name = "base.html"
 
 
-class ManageCourseListView(ListView):
-    model = Course
+class ManageCourseListView(OwnerCourseMixin, ListView):
     template_name = 'courses/manage/course/list.html'
 
-    def get_queryset(self):
-        qs = super().get_queryset()
-        return qs.filter(owner=self.request.user)
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
     
