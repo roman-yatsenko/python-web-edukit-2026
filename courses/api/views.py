dd0d8e75d7a22923a0c61cab13aae7caa808768a
YaRo <roman.yatsenko@hneu.net>
2026-05-20 21:00:20 +0300

    Add CourseEnrollView for course enrollment functionality

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,8 +1,22 @@
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from courses.api.serializers import SubjectSerializer
-from courses.models import Subject
+from courses.models import Subject, Course
 
+
+class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
