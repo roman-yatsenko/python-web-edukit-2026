eb6b8c0adaea344eff145afb04bd92a255032857
YaRo <roman.yatsenko@hneu.net>
2026-06-10 19:31:13 +0300

    Add enroll action to CourseViewSet for course enrollment functionality

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, viewsets
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -29,3 +30,12 @@ class SubjectDetailView(generics.RetrieveAPIView):
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    @action(detail=True, 
+            methods=['post'],
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
